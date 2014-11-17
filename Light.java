a8e97d6ddba871988cec605ba65ecc9a09dfc55f
Octoberapples <linneamd@gmail.com>
2014-11-17 15:27:24 +0100

    Senaste light

--- Light.java
+++ Light.java
@@ -2,27 +2,31 @@
 package simulering;
 
 /**
+ * 
  * The class {@code Light} contains and manages the traffic lights and their attributes in the traffic system.
  * 
  * @author Linnea Dahl and Axel Bostrom
  * 
  */
 public class Light {
-       private final int period;
+    private final int period;
     private int time; 
     private final int green;
     
     public Light(int period, int green) {
-        this.period = period - 1;
+        this.period = period;
         time = 0;
-        this.green = green - 1;
+        this.green = green;
     }
 
  public void step() { 
-        if(time<period) time++;
+        if(time<period-1) time++;
         else time = 0;
         
     }
+ public int getTime(){
+     return this.time;
+ }
 
     public boolean isGreen()   {
         return time<=green;
