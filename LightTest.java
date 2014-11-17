6f403dee9f7b4431298f0c9ecdc676c214732aed
Octoberapples <linneamd@gmail.com>
2014-11-17 12:14:40 +0100

    Update LightTest.java

--- LightTest.java
+++ LightTest.java
@@ -5,10 +5,6 @@
  */
 package simulering;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -21,21 +17,6 @@ public class LightTest {
     public LightTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of step method, of class Light.
@@ -62,19 +43,4 @@ public class LightTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of toString method, of class Light.
-     */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        Light instance = null;
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
